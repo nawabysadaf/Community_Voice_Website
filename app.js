7353427f44d4c3209c9645a5d28b52fbe4dd086a
EffatNawaby <sadafnawaby@gmail.com>
2025-03-13 20:34:45 +0100

    adding two new middlewares to the app.js file for the project and set the action of the forms in each html file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express, { request, response } from 'express'
-import {logger} from './middlewares/logger.js' 
+import { logger } from './middlewares/logger.js' 
 
 // importing and assigning the express and port number
 const app = express()
 const PORT = 3000
 
 app.use(logger)
+app.use('/community-voice', express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 // seting a route
 app.get('/', (request, response) => {
     response.send('Your first route is ready')
