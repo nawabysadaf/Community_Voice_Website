9358dcb377407c394a4f6af0eb05f9f489d4b096
EffatNawaby <sadafnawaby@gmail.com>
2025-03-13 13:20:16 +0100

    starting the first route successfully

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 // importing and assigning the express and port number
 const app = express()
 const PORT = 3000
+
+// seting a route
+app.get('/', (request, response) => {
+    response.send('Your first route is ready')
+})
 
 // starting the server
 app.listen(PORT, () => {
